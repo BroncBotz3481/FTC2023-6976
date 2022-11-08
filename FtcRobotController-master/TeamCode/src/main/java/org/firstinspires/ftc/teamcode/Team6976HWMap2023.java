f195d2f7c813fc04c0f523fb8fdb28bd5ffe30a1
4008 <4008teamBroncBotz@gmail.com>
2022-11-08 17:16:15 -0600

    Base Code 6976

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976HWMap2023.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Team6976HWMap2023 {
+    public DcMotor DriveRightBack = null;
+    public DcMotor DriveLeftBack = null;
+    public DcMotor DriveLeftFront = null;
+    public DcMotor DriveRightFront = null;
+
+    public DcMotor Elevator = null;
+    public Servo Intake = null;
+
+    HardwareMap hwMap =  null;
+
+    public void Map(HardwareMap hardwareMap)
+    {
+          hwMap = hardwareMap;
+          DriveLeftFront = hwMap.get(DcMotor.class,"DriveLeftFront");
+          DriveRightFront = hwMap.get(DcMotor.class,"DriveRightFront");
+          DriveLeftBack = hwMap.get(DcMotor.class,"DriveLeftBack");
+          DriveRightBack = hwMap.get(DcMotor.class,"DriveRightBack");
+
+          DriveLeftFront.setDirection(DcMotor.Direction.FORWARD);
+          DriveLeftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+          DriveLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+          DriveRightFront.setDirection(DcMotor.Direction.REVERSE);
+          DriveRightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+          DriveRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+          DriveLeftBack.setDirection(DcMotor.Direction.FORWARD);
+          DriveLeftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+          DriveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+          DriveRightBack.setDirection(DcMotor.Direction.REVERSE);
+          DriveRightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+          DriveRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+          Elevator = hwMap.get(DcMotor.class, "Elevator");
+          Elevator.setDirection(DcMotorSimple.Direction.REVERSE);
+          Elevator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+          Elevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+          Intake = hwMap.get(Servo.class, "Intake");
+    }
+
+}
---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2023New6976.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import android.text.method.Touch;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import org.firstinspires.ftc.teamcode.Team6976HWMap2023;
+
+public class TeleOp2023New6976 {
+    @TeleOp(name = "Team6976TeleOp2023", group = "4008")
+    public class TeleOp2023NewYes6976 extends LinearOpMode {
+        Team6976HWMap2023 robot = new Team6976HWMap2023();
+
+
+        @Override
+        public void runOpMode() {
+            robot.Map(hardwareMap);
+            telemetry.addData("Say", "TeleOp Starting");
+            telemetry.update();
+            robot.DriveRightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.DriveRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.DriveLeftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.DriveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.DriveRightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.DriveRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.Intake.setPosition(0);
+            robot.Intake.setPosition(1);
+
+            //robot.Lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BEATS_PER_MINUTE_PARTY_PALETTE);
+            waitForStart();
+
+            while (opModeIsActive()) {
+                boolean speedslow = gamepad1.right_bumper;
+                double mag = speedslow ? 1.0 : 0.5;
+
+                double y = gamepad1.left_stick_y; // Remember, this is reversed!
+                double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+                double rx = -gamepad1.right_stick_x;
+
+                // hello
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio, but only when
+                // at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+                double frontLeftPower = (y + x + rx) / denominator;
+                double backLeftPower = (y - x + rx) / denominator;
+                double frontRightPower = (y - x - rx) / denominator;
+                double backRightPower = (y + x - rx) / denominator;
+
+                telemetry.addData("RightFront", robot.DriveRightFront.getCurrentPosition());
+                telemetry.addData("RightBack", robot.DriveRightBack.getCurrentPosition());
+                telemetry.addData("LeftFront", robot.DriveLeftFront.getCurrentPosition());
+                telemetry.addData("LeftBack", robot.DriveLeftBack.getCurrentPosition());
+                telemetry.update();
+
+                robot.DriveLeftFront.setPower(frontLeftPower * mag);
+                robot.DriveLeftBack.setPower(backLeftPower * mag);
+                robot.DriveRightFront.setPower(frontRightPower * mag);
+                robot.DriveRightBack.setPower(backRightPower * mag);
+
+                if (gamepad2.b) {
+                    robot.Elevator.setPower(1);
+                }
+                else if (gamepad2.a){
+                    robot.Elevator.setPower(-1);
+                }
+                else {
+                    robot.Elevator.setPower(0);
+                }
+
+                if (gamepad2.x){
+                    robot.Intake.setPosition(1);
+                }
+                else{
+                    robot.Intake.setPosition(0);
+                }
+
+            }
+        }
+    }
+}
